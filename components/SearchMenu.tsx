6e761bfef45550be7debdf7864fda48cc0d6172c
Fatih HAS <fatihhas2609@gmail.com>
2024-01-04 11:38:34 +0300

    searchmodal close

--- components/SearchMenu.tsx
+++ components/SearchMenu.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import Input from './input';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 interface SearchMenuProps {
     visible?: boolean;
@@ -30,18 +31,23 @@ const SearchMenu: React.FC<SearchMenuProps> = ({
     }
 
     return (
-        <div className="z-50 transition duration-300 bg-black bg-opacity-80 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0">
+        <div onClick={handleClose} className="z-50 transition duration-300 bg-black bg-opacity-80 flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0">
             <div className="relative w-auto mx-auto max-w-3xl rounded-md overflow-hidden">
                 <div className={`${isVisible ? 'scale-100' : 'scale-0'} transform duration-300 relative flex-auto bg-zinc-900 drop-shadow-md`}>
 
-                    <div className="relative h-96">
-                        <Input
-                            id="name"
-                            type="text"
-                            label="Search"
-                            value={word}
-                            onChange={(e: any) => setWord(e.target.value)}
-                        />
+                    <div className="relative h-96 w-96">
+                        <div className="w-10">
+                            <Input
+                                id="name"
+                                type="text"
+                                label="Search"
+                                value={word}
+                                onChange={(e: any) => setWord(e.target.value)}
+                            />
+                        </div>
+                        <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+                            <MagnifyingGlassIcon className="w-6" />
+                        </div>
                     </div>
                 </div>
             </div>
